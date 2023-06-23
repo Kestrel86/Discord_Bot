df88da8f41cb4890b2c3fcd0726bf71f5fe2e2aa
Kestrel86 <valdezandrew20@gmail.com>
2023-06-22 21:44:52 -0700

    added requirements and completed base bot

--- bot.py
+++ bot.py
@@ -1,27 +1,29 @@
-from re import A
 import enum
-import discord
-import settings
-import random
+from logging import PlaceHolder
+from re import A
+import discord 
 from discord.ext import commands
-from discord import app_commands
+from discord import SelectOption, app_commands
+import settings
 import utils
-
+import typing
+import traceback
+    
 logger = settings.logging.getLogger("bot")
 
-# def is_certain_user():
+# def is_owner():
 #     def predicate(interaction : discord.Interaction):
-#         if interaction.user.id == "_Uptown":
+#         if interaction.user.id == interaction.guild.owner.id:
 #             return True
 #     return app_commands.check(predicate)
 
 def run():
     intents = discord.Intents.all()
-    intents.members = True
     intents.message_content = True
-
+    intents.members = True
+    
     bot = commands.Bot(command_prefix="!", intents=intents)
-
+    
     @bot.event 
     async def on_ready():
         #logger.info(f"Logged in as {bot.user} (ID: {bot.user.id})")
@@ -41,6 +43,7 @@ def run():
         await bot.tree.sync(guild=settings.GUILDS_ID)
         await utils.load_videocmds(bot)
 
+    # cog commands
     @bot.command()
     async def load(ctx, cog: str):
         await bot.load_extension(f"cogs.{cog.lower()}")
--- cogs/music.py
+++ cogs/music.py
@@ -3,64 +3,90 @@ from discord.ext import commands
 import settings 
 import wavelink
 
+
 logger = settings.logging.getLogger(__name__)
 
 class SB_Button(discord.ui.Button):
-    file_name : str = None
-
+    file_name : str = None 
+    
     def setup(self, data):
-        self.label = data["label"]
-        self.custom_id = data["custom_id"]
-        self.file_name = data["file_name"]
-
+        self.label = data['label']
+        self.custom_id = data['custom_id']
+        self.file_name = data['file_name']
+    
     async def callback(self, interaction: discord.Interaction):
         await interaction.response.defer()
-        soundboardItem = await wavelink.YouTubeTrack.search(query=self.file_name, return_first=True)
-        if soundboardItem:
-            await self.view.player.play(soundboardItem)
-
+        soundboard_item = await wavelink.YouTubeTrack.search(query=self.file_name, return_first=True)
+        if soundboard_item:
+            await self.view.player.play(soundboard_item)
+        
+    
 class SoundboardView(discord.ui.View):
-    player : wavelink.Player = None
-
-    @discord.ui.button(label="Stop", style=discord.ButtonStyle.red)
+    player : wavelink.Player = None 
+    
+    
+    @discord.ui.button(label="Stop", 
+                       style=discord.ButtonStyle.red)
     async def stop_button(self, interaction:discord.Interaction, button: discord.ui.Button):
         await interaction.response.defer()
         await self.player.stop()
-
-    def setupButtons(self):
-        #only 25 buttons total, 24 if stop button included
-        buttons = [
+    
+    def setup_buttons(self):
+        # Only up to 25 buttons in total
+        # with the stop button above, only 24 are possible
+        buttons = [ 
+               {
+                "label": "Applause",
+                "custom_id": "applause",
+                "file_name": "https://www.youtube.com/watch?v=jDOrc8FmDy4",
+            },
             {
                 "label": "Rizz",
-                "custom_id": "Rizz",
+                "custom_id": "rizz",
                 "file_name": "https://www.youtube.com/watch?v=mq9VoNZdJqg",
             },
             {
-                "label": "Bass",
-                "custom_id": "Bass",
-                "file_name": "https://www.youtube.com/watch?v=f8mL0_4GeV0",
+                "label": "Yamete Kudasai",
+                "custom_id": "yamete_kudasai",
+                "file_name": "https://www.youtube.com/watch?v=gb4ZnGIhPD0",
             },
             {
-                "label": "Fart",
-                "custom_id": "Fart",
-                "file_name": "https://www.youtube.com/watch?v=ZQn8sQjTO0M",
+                "label": "Deez Nuts",
+                "custom_id": "deez_nuts",
+                "file_name": "https://www.youtube.com/watch?v=66I78hXXwvk",
+            },
+            {
+                "label": "Thomas Tank Engine",
+                "custom_id": "thomas_tank",
+                "file_name": "https://www.youtube.com/watch?v=gvTcmoJDVjg",
+            },
+            {
+                "label": "Oof",
+                "custom_id": "oof",
+                "file_name": "https://www.youtube.com/watch?v=3w-2gUSus34",
+            },
+            {
+                "label": "Do it",
+                "custom_id": "do_it",
+                "file_name": "https://www.youtube.com/watch?v=z2Qe1d4urfw",
             }
         ]
-        for buttonConfig in buttons:
+        
+        for button_config in buttons:
             sb_button = SB_Button()
-            sb_button.setup(buttonConfig)
+            sb_button.setup(button_config)
             self.add_item(sb_button)
-
+        
 class MusicBot(commands.Cog):
     vc : wavelink.Player = None
     current_track = None
     music_channel = None
     history = None
-
+    
     def __init__(self, bot):
         self.bot = bot
         self.history = list()
-
+        
     async def setup(self):
         """
         Sets up a connection to lavalink
@@ -69,104 +95,79 @@ class MusicBot(commands.Cog):
             bot=self.bot, 
             host="localhost",
             port=2333, 
-            password="Password"
+            password="changeme"
         )
-
+    
+    #events
     @commands.Cog.listener()
     async def on_wavelink_node_ready(self, node: wavelink.Node):
-        logger.info(f"{node} ready")
-
-    @commands.Cog.listener()
-    async def on_wavelink_track_start(self, player: wavelink.Player, track: wavelink.Track):
-        await self.music_channel.send(f"Now playing: {track.title}")
-
+        logger.info(f"{node} is ready")
+    
+    # @commands.Cog.listener()
+    # async def on_wavelink_track_start(self, player: wavelink.Player, track: wavelink.Track):
+    #     await self.music_channel.send(f"{track.title} started playing")
+        
     @commands.Cog.listener()
     async def on_wavelink_track_end(self, player: wavelink.Player, track: wavelink.Track, reason):
-        await self.music_channel.send(f"Finished: {track.title}")
-        self.history.append(track.title)
+        if not player.queue.is_empty:
+            next_track = player.queue.get()
+            await player.play(next_track)
 
-    #soundboard 
-    @commands.command(brief="Opens a soundboard with buttons")
-    async def sb(self, ctx):
-        view = SoundboardView(timeout=None)
-        view.setup_buttons()
-        view.player = self.vc
-        await ctx.send("Your soundboard", view=view)
-
-    @commands.command(brief="Joins bot into VC user is currently present in")
+    #commands
+    
+    @commands.command(brief="Manually joins the bot into the voice channel")
     async def join(self, ctx):
         channel = ctx.message.author.voice.channel
         self.music_channel = ctx.message.channel
-        if not channel: 
-            await ctx.send("You need to be in a voice channel to use this command")
-            return
+        if not channel:
+            await ctx.send(f"You need to join a voice channel first.")
+            return 
         self.vc = await channel.connect(cls=wavelink.Player)
-        await ctx.send(f"Connected to {channel.name}")
+        await ctx.send(f"Joined {channel.name}")
 
-    @commands.command(brief="Leaves bot from VC")
-    async def leave(self, ctx):
-        channel = ctx.message.author.voice.channel
-        if not channel: 
-            await ctx.send("You need to be in a voice channel to use this command")
-            return
-        self.vc = await channel.disconnect(cls=wavelink.Player)
-        await ctx.send(f"Connected to {channel.name}")
-        
-    @commands.command(brief="Search for a track from YT, SoundCloud, or Spotify")
-    async def add(self, ctx, *title : str):
-        #!search yt
-        chosen_track = await wavelink.YouTubeTrack.search(query=" ".join(title), return_first=True)
-        if chosen_track:
-            self.current_track = chosen_track
-            await ctx.send(f"Added {chosen_track.title} to the queue")
-            self.vc.queue.put(chosen_track)
+    @commands.command()
+    async def play(self, ctx, *, search: wavelink.YouTubeTrack):
+        if not self.vc:
+            self.vc = await ctx.author.voice.channel.connect(cls=wavelink.Player)
 
-    @commands.command(brief="Plays the current track in queue")
-    async def play(self, ctx):
-        if self.current_track and self.vc:
-            await self.vc.play(self.current_track)
-            await ctx.send(f"Playing {self.current_track.title}")
-    
-    @commands.command(brief="Skips current song")
+        if self.vc.is_playing():
+
+            self.vc.queue.put(item=search)
+        else:
+            await self.vc.play(search)
+            
+    @commands.command(brief="Skips the current song")
     async def skip(self, ctx):
         if self.vc.queue.is_empty:
-            await ctx.send("Queue is empty")
-            return
+            await ctx.send("There are no more tracks!")
+            return 
         self.current_track = self.vc.queue.get()
         await self.vc.play(self.current_track)
-
-    @commands.command(brief="Pauses the current song")
+    
+    @commands.command(brief="Pause playing song")
     async def pause(self, ctx):
-        await self.vc.pause(self.current_track)   
-        await ctx.send(f"Paused {self.current_track.title}")
-
-    @commands.command(brief="Resumes the current song")
+        await self.vc.pause()
+        await ctx.send(f"Paused current Track")            
+        
+    @commands.command(brief="Resumes current paused song")
     async def resume(self, ctx):
-        await self.vc.resume(self.current_track)
-        await ctx.send(f"Resumed {self.current_track.title}")
-
-    @commands.command(brief="Stops the current song")
+        await self.vc.resume()
+        await ctx.send(f"Resuming current Track")
+        
+    @commands.command(brief="Stops current song")
     async def stop(self, ctx):
-        await self.vc.stop(self.current_track)
-        await ctx.send(f"Stopped {self.current_track.title}")
-
-    @commands.command(brief="Fast Forward")
+        await self.vc.stop()
+        
+    
+    @commands.command(brief="Fast Forward n seconds")
     async def ff(self, ctx, seconds : int = 15):
         new_position = self.vc.position + seconds
         await self.vc.seek(new_position * 1000)
-
-    @commands.command(brief="Rewind")
-    async def rw(self, ctx, seconds : int = 15):
+        
+    @commands.command(brief="Go back n seconds")
+    async def gb(self, ctx, seconds : int = 15):
         new_position = self.vc.position - seconds
         await self.vc.seek(new_position * 1000)
-
-    @commands.command(brief="Set Volume")
-    async def volume(self, ctx, volume : int = 100):
-        await self.vc.set_volume(volume)
-
-    @commands.command(brief="Get Volume")
-    async def getvolume(self, ctx):
-        await ctx.send(f"Volume: {self.vc.volume}")
     
     @commands.command(brief="Shows song history")
     async def history(self, ctx):
@@ -175,11 +176,27 @@ class MusicBot(commands.Cog):
         for song in self.history:
             track_info = song.split(" - ")
             embed.add_field(name=track_info[1], value=track_info[0], inline=False)
+
         await ctx.send(embed=embed)
 
+    @commands.command(brief="Leaves current voice channel")
+    async def leave(self, ctx):
+        if self.vc:
+            await self.vc.disconnect()
+            await ctx.send(f"Left {self.vc.channel.name}")
+        else:
+            await ctx.send("Not in a voice channel")
+            
+    
+    @commands.command(brief="Opens a soundboard with buttons")
+    async def sb(self, ctx):
+        view = SoundboardView(timeout=None)
+        view.setup_buttons()
+        view.player = self.vc
+        await ctx.send("Your soundboard", view=view)
+        
 async def setup(bot):
     music_bot = MusicBot(bot)
     await bot.add_cog(music_bot)
     await music_bot.setup()
 
-    
